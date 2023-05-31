cca2d8553ca5f1de872d1209a58f2b0c5596abdf
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2023-06-01 00:10:53 +0100

    working on home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 import useAuth from "../hooks/useAuth";
 import url from "../api/baseURL";
+import Conversation from "../components/Conversation";
+import { TextField } from "@mui/material";
 
 export default function HomePage() {
     const { user, token } = useAuth();
+    const [message, setMessage] = useState("");
 
     return (
         <>
@@ -17,20 +20,23 @@ export default function HomePage() {
                         <button>Create Group Chat</button>
                     </div>
                     <div className="conversations">
-                        <div className="conversation">
-                            <img src={`${url}/images/profile.png`} />
-                            <div className="wrapper">
-                                <div className="information">
-                                    <div className="user-name">
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
                     </div>
                 </div>
                 <div className="messages-container">
+                    <div className="chat-box">
 
+                    </div>
+                    <form autoComplete="off" >
+                        <input value={message} onChange={(e: any) => { setMessage(e.target.value) }} placeholder="enter message..." className="field" />
+                        <button>Send message</button>
+                    </form>
                 </div>
             </section>
             <Toaster />
