f6da9d8c220415dd4c536592f9606d4e15697d24
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2023-07-09 17:57:47 +0100

    working on home page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,13 +4,33 @@ import url from "../api/baseURL";
 import useAuth from "../hooks/useAuth";
 import { FaSearch } from "react-icons/fa";
 
-export default function Navbar(){
-    const { user , token } = useAuth();
+export default function Navbar() {
+    const { user, token } = useAuth();
 
-    return(
+    return (
         <>
-            <nav>
+            <nav className="shadow" >
+                <div className="container">
+                    <div className="website-container">
+                        <img src={logo} alt="logo-picture" />
+                        <div className="search-container">
+                            <FaSearch className="icon" />
+                            <input placeholder="Type here to search..." />
+                        </div>
+                    </div>
+                    <div className="profile-container">
 
+                        <div className="avatar-wrapper">
+                            <img src={`${url}/images/profile.png`} alt="user-picture" />
+                            <div className="dropdown">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="placeholder">
+
+                </div>
             </nav>
         </>
     )
